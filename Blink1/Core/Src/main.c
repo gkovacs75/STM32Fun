dd74d3d699562cd358c8dab79bda0a3dcba783cb
gkovacs75 <github.gomb0@passmail.net>
2026-07-10 12:32:32 -0400

    wip

--- Blink1/Core/Src/main.c
+++ Blink1/Core/Src/main.c
@@ -113,7 +113,7 @@ int main(void)
 	MX_GPIO_Init();
 	MX_USART2_UART_Init();
 	/* USER CODE BEGIN 2 */
-	printf("Starting...\r\n");
+	printf("Starting Blink1...\r\n");
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
